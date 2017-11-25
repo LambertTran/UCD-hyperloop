6ea56626c4bc18df0b9df22c7ef58e93d9535c89
Lambert Tran <lb.tran648@gmail.com>
2017-11-24 20:56:43 -0800

    task runner for css, handlebars, and js

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del  = require('del');
+var rename = require('gulp-rename');
+var liveReLoad = require('gulp-livereload');
+var browserSync= require('browser-sync');
+
+/** clean up css files **/
+gulp.task('clean', () => {
+  del(['public/css/index.css']); 
+});
+
+/** setup browser for reloading **/
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    proxy:"localhost:8080",
+    browser:"google chrome",
+    localOnly:true
+  });
+});
+
+/** reload the browser **/
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+/** watch anything change to reload **/
+gulp.task('watch',['browser-sync'], () => {
+  gulp.watch('./public/css/*.css',['reload']);
+  gulp.watch('./views/*.handlebars',['reload']);
+  gulp.watch('./routes/**/*.js',['reload']);
+})
+
