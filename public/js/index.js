2216145cf3d88c4fcae3e1e6d7ff4e2f968aa6d2
Lambert Tran <lb.tran648@gmail.com>
2017-12-18 18:11:08 -0800

    add logo section in header

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
-/** active navigation bar for admin page **/
+/** active navigation bar for admin **/
 // navbarActivate("nav-admin","nav-admin-active");
-/** active navbar for  **/
+/** active navbar for client site **/
 navbarActivate("nav-client","nav-client-active");
 
+
+// add active class to navbar
 function navbarActivate(element,activeClass){
-  var ul = document.getElementById(element).getElementsByTagName('a');
+  var ul = document.getElementById(element).getElementsByTagName('li');
   for(var i=0; i<ul.length; ++i){
     ul[i].addEventListener("click",addActiveClass);
   }
