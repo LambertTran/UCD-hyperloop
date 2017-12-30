8338f0a6bea702dcf0713f51d93d0bbae18865be
Lambert Tran <lb.tran648@gmail.com>
2017-12-30 12:07:55 -0800

    add identity folder

---
+++ identity/db-identity.js
@@ -0,0 +1,13 @@
+
+/*
+ * Configuration of mysql database in AWS RDB 
+ */
+
+var dbIdentity = {
+  host:'mysqlinstance.cqel7ccv9iks.us-west-1.rds.amazonaws.com',
+  user: 'alirom93',
+  password: 'Unknown123',
+  database: 'test'
+}
+
+module.exports = dbIdentity;
