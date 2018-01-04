f9cc2868219e7b8db0e0d21f17cc8a487d5a330b
Lambert Tran <lb.tran648@gmail.com>
2018-01-04 10:03:30 -0800

    test out database 1

--- routes/database/mysql-db.js
+++ routes/database/mysql-db.js
@@ -8,11 +8,12 @@ const fs = require('fs');
 
 // database identidy
 let dbIdentity;
-try{
-  dbIdentity = require('../../identity/db-identity');
-} catch(e){
-  dbIdentity = require('../../identity-heroku/heroku-db-identity');
-}
+// try{
+//   dbIdentity = require('../../identity/db-identity');
+// } catch(e){
+//   dbIdentity = require('../../identity-heroku/heroku-db-identity');
+// }
+dbIdentity = require('../../identity-heroku/heroku-db-identity');
 
 /** =================================
                 Body
